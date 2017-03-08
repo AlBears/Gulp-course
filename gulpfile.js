f7a53681d1a91064b852032e02f923d5188d36b4
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-03-08 12:55:44 +0300

    production refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,61 @@
 var gulp = require('gulp'),
     $ = require('gulp-load-plugins')();
 
+const config = {
+  styles: {
+    src: ['./src/styles/site.less'],
+    dest: './public/styles/',
+    autoprefix: ['last 2 versions']
+  },
+
+  scripts: {
+    src: ['./src/scripts/**/*.js'],
+    dest: './public/scripts',
+    bundle: 'app.js'
+  }
+}
+
 gulp.task("dev:styles", styles(false));
 gulp.task("dev:scripts", devScripts);
 
-gulp.task('prod:styles', styles(true))
+gulp.task('dev', gulp.parallel('dev:styles', 'dev:scripts'));
 
-gulp.task('default',
-    gulp.parallel
-    (
-      'dev:styles',
-      devScripts
-    )
-  );
+gulp.task('prod:styles', styles(true));
+gulp.task('prod:scripts', prodScripts);
+
+gulp.task('prod', gulp.parallel('prod:styles', 'prod:scripts'));
+
+gulp.task('default', gulp.series('dev'));
 
 function styles(isProduction) {
   return () => {
   return gulp
-      .src('./src/styles/site.less')
+      .src(config.styles.src)
       .pipe($.if(!isProduction, $.sourcemaps.init()))
       .pipe($.less())
       .pipe($.autoprefixer({
-        browsers: ['last 2 versions']
+        browsers: config.styles.autoprefix
       }))
       .pipe($.if(isProduction, $.minifyCss()))
       .pipe($.if(!isProduction, $.sourcemaps.write()))
-      .pipe(gulp.dest('./public/styles'));
+      .pipe(gulp.dest(config.styles.dest));
     }
 }
 
 function devScripts() {
   return gulp
-      .src('./src/scripts/**/*.js')
+      .src(config.scripts.src)
       .pipe($.sourcemaps.init())
       .pipe($.babel())
       .pipe($.sourcemaps.write('.'))
-      .pipe(gulp.dest('./public/scripts'));
+      .pipe(gulp.dest(config.scripts.dest));
+}
+
+function prodScripts() {
+  return gulp
+      .src(config.scripts.src)
+      .pipe($.babel())
+      .pipe($.concat(config.scripts.bundle))
+      .pipe($.uglify())
+      .pipe(gulp.dest(config.scripts.dest));
 }
